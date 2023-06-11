90ab50d83e50ebd44e5ad12ea3f55524ac0b7caa
Ítallo Winícios <italloferreirainfo@gmail.com>
2023-06-10 21:09:47 -0300

    modifications

--- TESTE_minicurso/migrations/2_deploy_contracts.js
+++ TESTE_minicurso/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Marketplace = artifacts.require("Marketplace")
+const NFT = artifacts.require("NFT")
 
 module.exports = function (deployer) {
     deployer.deploy(Marketplace);
+    deployer.deploy(NFT, Marketplace.address);
 };
